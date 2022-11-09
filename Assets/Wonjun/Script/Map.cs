703be0d5b01b2a51dde879ca3a3a26a8eefa1f7e
준1 <98933515+Wizone06@users.noreply.github.com>
2022-11-09 10:53:48 +0900

    맵 해금 초반
    맵 해금 초반

--- Assets/Wonjun/Script/Map.cs
+++ Assets/Wonjun/Script/Map.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
     [SerializeField] private GameObject[] map;
-    public void Update()
+    public void FirstMap()
     {
-        
+        map[1].SetActive(false);
     }
-    public void MapOpen()
+    public void SecondMap()
     {
-        
+        map[2].SetActive(false);
+        map[3].SetActive(false);
     }
+    public void thirdMap()
+    {
+        map[6].SetActive(false);
+    }
+    public void fourMap()
+    {
+        map[4].SetActive(false);
+        map[5].SetActive(false);
+
+    }
+    
+    
+
 
 }
